9731fcffd222eb8c1fa069af1e96354bf39ef723
kentarou-okamoto <45345280+kentarou-okamoto@users.noreply.github.com>
2024-09-27 16:40:51 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -53,8 +53,8 @@ def main():
 
            #千円の場合100の単位で切り捨て
            if unit =='千円':
-              df['2'] = df['f2'].apply(lambda x: x // 10)  
-              df['3'] = df['f3'].apply(lambda x: x // 10)  
+              df['2'] = df['f2'].apply(lambda x: (x//10) )  
+              df['3'] = df['f3'].apply(lambda x: (x//10) )  
 
            #列の削除
            if vle_clm =='右列':
