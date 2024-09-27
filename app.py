873fc9290291c5277e5471e936b72d3211f6d3c7
kentarou-okamoto <45345280+kentarou-okamoto@users.noreply.github.com>
2024-09-27 17:20:42 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def main():
       
            #千円の場合100の単位で切り捨て
            if unit =='千円':
-              df['f_vle'] = df['f_vle'].astype('float64') / 100
+              df['f_vle'] = df['f_vle'].astype('int') // 100
               
       #df['f_vle'] = df['f_vle'].map(lambda x: x+2  , na_action='ignore') 
               
