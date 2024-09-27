f4356f01d9ab138e024170572eaf5bcdf8367174
kentarou-okamoto <45345280+kentarou-okamoto@users.noreply.github.com>
2024-09-27 15:07:40 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -50,8 +50,8 @@ def main():
            st.table(df)
 
            col1, col2 = st.columns(2)
-           col1.dataframe(df["f1"], hide_index=True)
-           col2.dataframe(df["f3"], hide_index=True)
+           col1.dataframe(df["f1"], hide_index=False)
+           col2.dataframe(df["f3"], hide_index=False)
            
 
 
