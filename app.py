2318d0cf72a6b5b04a5e2b1d5b2b59acab034e02
kentarou-okamoto <45345280+kentarou-okamoto@users.noreply.github.com>
2024-09-27 18:17:52 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -62,6 +62,10 @@ def main():
            df = df.reset_index(drop=True)
            df.index = df.index + 1
       
+           df['f_vle'] = df['f_vle'].replace('-', '')
+           df['f_vle'] = df['f_vle'].replace('△', '-')
+           df['f1'] = df['f_vle'].replace('1株', '１株')
+           
            #千円の場合100の単位で切り捨て
            if unit =='千円':
               df['f_vle'] = df['f_vle'].astype('int') // 1000
