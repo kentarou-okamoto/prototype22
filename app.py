de255419a094ba3e25375ac8e30cf7ce36d949ad
kentarou-okamoto <45345280+kentarou-okamoto@users.noreply.github.com>
2024-09-27 12:55:27 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st
 
-st.title("hello")
+st.title("入力フォーム")
+
+
+# テキスト入力ボックス
+text_input = st.text_input('Input', 'Input some text here.')
+# テキストエリア
+text_area = st.text_area('Text Area', 'Input some text here.')
 
