535525ef0ef6150fefa0dd5417c6760d0625f9f9
kentarou-okamoto <45345280+kentarou-okamoto@users.noreply.github.com>
2024-09-27 13:27:28 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ st.header('入力フォーム')
 #text_input = st.text_input('Input', 'Input some text here.')
 
 sample_text = """資産の部
-    流動資産
-    現金及び預金 98,765 43,210
-    売掛金 12,345 67,890
+　　流動資産
+　　　　現金及び預金　　　98,765　　　43,210
+　　　　売掛金　　　　　　12,345　　　67,890
 """
 
 # テキストエリア
 with st.form("my_form", clear_on_submit=False):
-     text_area = st.text_area('Text Area', sample_text , height=100)
+     text_area = st.text_area('Text Area', sample_text , height=500)
      submitted = st.form_submit_button("文字列を変換")
      
      
