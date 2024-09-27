2c6a9ac686fa69402aa8b023194318d389cd5ff4
kentarou-okamoto <45345280+kentarou-okamoto@users.noreply.github.com>
2024-09-27 14:40:26 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -47,9 +47,9 @@ def main():
            st.table(df)
 
            col1, col2 = st.columns(2)
-           #df2 = df["f1"]
+           df2 = df["f1"]
            
-           col1 = st.write(df["f1"].hide(axis="index") )
+           col1 = st.write(df2 )
            col2 = st.write(df["f3"].values.tolist())
 
 
