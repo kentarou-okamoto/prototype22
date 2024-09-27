d371b38aaddf7c64a08c96c61bfc3164b1d3f60d
kentarou-okamoto <45345280+kentarou-okamoto@users.noreply.github.com>
2024-09-27 13:06:19 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ st.title("入力フォーム")
 
 
 # テキスト入力ボックス
-text_input = st.text_input('Input', 'Input some text here.')
+#text_input = st.text_input('Input', 'Input some text here.')
 # テキストエリア
 text_area = st.text_area('Text Area', 'Input some text here.')
 
+import time
+
+# ボタンを押したら3秒間出力を待つ
+if st.button('start'):
+    with st.spinner('processiong...'):
+        time.sleep(3)
+        st.write('押したなぁ～')
+        st.write('end!')
