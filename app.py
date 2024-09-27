f7476113f6b5a159e3500cdc6f33b4fdc5734426
kentarou-okamoto <45345280+kentarou-okamoto@users.noreply.github.com>
2024-09-27 13:33:19 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ sample_text = """資産の部
 
 # テキストエリア
 with st.form("my_form", clear_on_submit=False):
-     text_area = st.text_area('Text Area', sample_text , height=200,disabled=True)
+     text_area = st.text_area('Text Area', placeholder = sample_text , height=200)
      submitted = st.form_submit_button("文字列を変換")
      
      
