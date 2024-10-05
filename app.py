dc773bea8e0ba26f0505482c3d15559eaaeb942c
kentarou-okamoto <45345280+kentarou-okamoto@users.noreply.github.com>
2024-10-05 18:33:33 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def main():
         text_area = st.text_area('Text Area', sample_text , height=200)
         col1, col2, col3 = st.columns(3)
         unit = col1.radio("単位", ('百万円', '千円'), index=0, horizontal=True)
-        vle_clm = col2.radio("取得する数字列", ('真ん中列', '右列''), index=1, horizontal=True)
+        vle_clm = col2.radio("取得する数字列", ('真ん中列', '右列'), index=1, horizontal=True)
         oo = st.checkbox("'1列のみ[削除しない]")
         submitted = col3.form_submit_button("文字列を変換")
      
