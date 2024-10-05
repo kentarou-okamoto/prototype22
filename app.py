9cb6ff5fa23829ff9299df75a490852de93b24a1
kentarou-okamoto <45345280+kentarou-okamoto@users.noreply.github.com>
2024-10-05 18:20:36 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -76,6 +76,8 @@ def main():
               time.sleep(1)
               if x==3:
                  del df['f2']
+                 
+           st.table(df)
               
            df.columns = ['f1', 'f_vle' ]
            df = df.dropna(subset=['f_vle'], axis=0)  #f_vle列の値がNoneの行(e.g.文字列Aetc...)を削除
