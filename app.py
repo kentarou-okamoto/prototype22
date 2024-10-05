46f622c0152eeea69059b0fd9d8f6728467331fe
kentarou-okamoto <45345280+kentarou-okamoto@users.noreply.github.com>
2024-10-05 18:28:02 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -76,8 +76,9 @@ def main():
               time.sleep(1)
               if x==3:
                  del df['f2']
+                 st.warning('1列のみを選択したのに数字列が２列あり!')
                  
-           st.table(df)
+           #st.table(df)
               
            df.columns = ['f1', 'f_vle' ]
            df = df.dropna(subset=['f_vle'], axis=0)  #f_vle列の値がNoneの行(e.g.文字列Aetc...)を削除
