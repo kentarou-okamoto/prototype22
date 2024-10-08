49064598dc5a55a898367b2358524d0eb494b891
kentarou-okamoto <45345280+kentarou-okamoto@users.noreply.github.com>
2024-10-08 21:14:53 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -94,14 +94,14 @@ def main():
            df.index = df.index + 1
       
            #数字列の整形:ダーシ/△/カッコ
-           df['f_vle'] = df['f_vle'].replace('-', '')
+           df['f_vle'] = df['f_vle'].replace('-', 'nan')
            df['f_vle'] = df['f_vle'].replace('△', '-', regex=True)
            #df['f_vle'] = df['f_vle'].replace('(', '-')
            #df['f_vle'] = df['f_vle'].replace(')', '')
       
            #千円の場合100の単位で切り捨て
            if unit =='千円':
-              df['f_vle'] = df['f_vle'].astype('int').apply(fn_devdev)
+              df['f_vle'] = df['f_vle'].astype('nan').apply(fn_devdev)
               
            df['f_vle'] = df['f_vle'].replace('', '-0')
            
